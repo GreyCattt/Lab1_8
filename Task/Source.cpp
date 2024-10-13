fcf243fa543f22cfbcaf7d0aaef650bcc42b7185
GreyCattt <rudiy545256@gmail.com>
2024-10-13 19:18:59 +0300

    Update Source.cpp
    remake task

--- Task/Source.cpp
+++ Task/Source.cpp
@@ -6,47 +6,17 @@
 */
 #include <iostream>
 using namespace std;
-int main() 
-{
-	int nA;
-	cout << "enter the number to check it for parity: ";
-	cin >> nA;
-	int choise;
-	cout << "chose parity check method 1, 2: ";
-	cin >> choise;
-	if (choise == 1) {
-		//first solution
-		if (nA % 2 == 0){
+int main() {
+    int number;
+    cout << "Enter the number to check: ";
+    cin >> number;
 
-			nA *= 2;
+    // Check the last bit of the number using the bitwise AND operation
+    if (number & 1) {
+        cout << "The number is odd." << endl;
+    } else {
+        cout << "The number is even." << endl;
+    }
 
-		}
-		cout <<"You chose method 1 and if it parity the number will double: " << nA;
-	}
-
-	else if (choise == 2) {
-		//second solution
-		
-		if (nA % 2 == 0)
-
-		{
-
-			nA *= 2;
-
-		}
-
-		else
-
-		{
-
-			nA--;
-
-		}
-		cout << "You chose method 2 and if it parity the number will double, else it will decrease by one: " << nA;
-	}
-	else
-	{
-		cout << "Wrong";
-	}
-	return 0;
+    return 0;
 }
